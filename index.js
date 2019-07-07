6912615a972db4c6098fd3133fd877bde7c26290
admin <gnyawali.sanjeev@gmail.com>
2019-07-07 22:51:42 +0545

    Add food handler route and table

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
 const userHandler = require("./handler/user-handler");
+const foodHandler = require("./handler/food-handler");
+
 const app = express();
 
 app.use(cors());
@@ -16,6 +18,13 @@ app.post("/user/login", userHandler.login);
 app.get("/user/:id", userHandler.getUserById);
 app.put("/user/upload/change/:id", userHandler.changeProfileImage);
 
+//API Food
+app.post("/food/upload", foodHandler.uploadFoodImage);
+app.post("/food", foodHandler.addFood);
+app.get("/food", foodHandler.getFood);
+app.get("/food/:category", foodHandler.getFoodByCategory);
+app.put("/food/:id", foodHandler.updateFood);
+app.delete("/food/:id", foodHandler.deleteFood);
 
 
 app.listen(3000, () => console.log("Server running on port 3000"));
